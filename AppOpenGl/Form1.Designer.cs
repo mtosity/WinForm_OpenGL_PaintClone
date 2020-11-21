bfadd2e24abe9234f1940fa094da62d6c20a4504
mtosity <mtosity@gmail.com>
2020-11-21 16:06:44 +0700

    draw simple polygon

---
+++ AppOpenGl/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace AppOpenGl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openGLControl = new SharpGL.OpenGLControl();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.hexagonRadio = new System.Windows.Forms.RadioButton();
+            this.pentagonRadio = new System.Windows.Forms.RadioButton();
+            this.triangleRadio = new System.Windows.Forms.RadioButton();
+            this.elipseRadio = new System.Windows.Forms.RadioButton();
+            this.rectangleRadio = new System.Windows.Forms.RadioButton();
+            this.circleRadio = new System.Windows.Forms.RadioButton();
+            this.lineRadio = new System.Windows.Forms.RadioButton();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.DrawFPS = false;
+            this.openGLControl.Location = new System.Drawing.Point(0, 1);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(667, 450);
+            this.openGLControl.TabIndex = 0;
+            this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            this.openGLControl.Resized += new System.EventHandler(this.openGLControl_Resized);
+            this.openGLControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseDown);
+            this.openGLControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseMove);
+            this.openGLControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.openGLControl_MouseUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.hexagonRadio);
+            this.groupBox1.Controls.Add(this.pentagonRadio);
+            this.groupBox1.Controls.Add(this.triangleRadio);
+            this.groupBox1.Controls.Add(this.elipseRadio);
+            this.groupBox1.Controls.Add(this.rectangleRadio);
+            this.groupBox1.Controls.Add(this.circleRadio);
+            this.groupBox1.Controls.Add(this.lineRadio);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(684, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(128, 261);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Draw Options";
+            // 
+            // hexagonRadio
+            // 
+            this.hexagonRadio.AutoSize = true;
+            this.hexagonRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hexagonRadio.Location = new System.Drawing.Point(6, 220);
+            this.hexagonRadio.Name = "hexagonRadio";
+            this.hexagonRadio.Size = new System.Drawing.Size(91, 24);
+            this.hexagonRadio.TabIndex = 6;
+            this.hexagonRadio.Text = "Hexagon";
+            this.hexagonRadio.UseVisualStyleBackColor = true;
+            // 
+            // pentagonRadio
+            // 
+            this.pentagonRadio.AutoSize = true;
+            this.pentagonRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pentagonRadio.Location = new System.Drawing.Point(7, 190);
+            this.pentagonRadio.Name = "pentagonRadio";
+            this.pentagonRadio.Size = new System.Drawing.Size(96, 24);
+            this.pentagonRadio.TabIndex = 5;
+            this.pentagonRadio.Text = "Pentagon";
+            this.pentagonRadio.UseVisualStyleBackColor = true;
+            // 
+            // triangleRadio
+            // 
+            this.triangleRadio.AutoSize = true;
+            this.triangleRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.triangleRadio.Location = new System.Drawing.Point(6, 140);
+            this.triangleRadio.Name = "triangleRadio";
+            this.triangleRadio.Size = new System.Drawing.Size(106, 44);
+            this.triangleRadio.TabIndex = 4;
+            this.triangleRadio.Text = "Equilateral \r\nTriangle";
+            this.triangleRadio.UseVisualStyleBackColor = true;
+            // 
+            // elipseRadio
+            // 
+            this.elipseRadio.AutoSize = true;
+            this.elipseRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.elipseRadio.Location = new System.Drawing.Point(6, 110);
+            this.elipseRadio.Name = "elipseRadio";
+            this.elipseRadio.Size = new System.Drawing.Size(70, 24);
+            this.elipseRadio.TabIndex = 3;
+            this.elipseRadio.Text = "Elipse";
+            this.elipseRadio.UseVisualStyleBackColor = true;
+            // 
+            // rectangleRadio
+            // 
+            this.rectangleRadio.AutoSize = true;
+            this.rectangleRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rectangleRadio.Location = new System.Drawing.Point(6, 80);
+            this.rectangleRadio.Name = "rectangleRadio";
+            this.rectangleRadio.Size = new System.Drawing.Size(100, 24);
+            this.rectangleRadio.TabIndex = 2;
+            this.rectangleRadio.Text = "Rectangle";
+            this.rectangleRadio.UseVisualStyleBackColor = true;
+            // 
+            // circleRadio
+            // 
+            this.circleRadio.AutoSize = true;
+            this.circleRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circleRadio.Location = new System.Drawing.Point(7, 50);
+            this.circleRadio.Name = "circleRadio";
+            this.circleRadio.Size = new System.Drawing.Size(66, 24);
+            this.circleRadio.TabIndex = 1;
+            this.circleRadio.Text = "Circle";
+            this.circleRadio.UseVisualStyleBackColor = true;
+            // 
+            // lineRadio
+            // 
+            this.lineRadio.AutoSize = true;
+            this.lineRadio.Checked = true;
+            this.lineRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lineRadio.Location = new System.Drawing.Point(7, 20);
+            this.lineRadio.Name = "lineRadio";
+            this.lineRadio.Size = new System.Drawing.Size(57, 24);
+            this.lineRadio.TabIndex = 0;
+            this.lineRadio.TabStop = true;
+            this.lineRadio.Text = "Line";
+            this.lineRadio.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(681, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Choose Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(681, 411);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(681, 359);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(131, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(681, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Line Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(681, 392);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Time Exucuted";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 453);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.openGLControl);
+            this.Name = "Form1";
+            this.Text = "OpenGL Paint";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton circleRadio;
+        private System.Windows.Forms.RadioButton lineRadio;
+        private System.Windows.Forms.RadioButton triangleRadio;
+        private System.Windows.Forms.RadioButton elipseRadio;
+        private System.Windows.Forms.RadioButton rectangleRadio;
+        private System.Windows.Forms.RadioButton hexagonRadio;
+        private System.Windows.Forms.RadioButton pentagonRadio;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AppOpenGl/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace AppOpenGl
+{
+    public partial class Form1 : Form
+    {
+        private Point pStart, pEnd;
+        private bool started = false, ended = false;
+        private Stopwatch watch;
+        private Stopwatch stopwatch = new Stopwatch();
+        public const int DRAW_LINE = 0, DRAW_CIRCLE = 1, DRAW_RECTANGLE = 2, DRAW_TRIANGLE = 3, DRAW_ELIPSE = 4, DRAW_PENTAGON = 5, DRAW_HEXAGON = 6;
+        public const double pi = 3.1415926f;
+
+        private int getDrawOption()
+        {
+            /*
+             0: Line
+             1: Circle
+             2: Square
+             3: Elipse
+             4: Equilateral Triangle
+             5: Pentagon
+             6: Hexagon
+            */
+            if (lineRadio.Checked)
+            {
+                return DRAW_LINE;
+            } else if (circleRadio.Checked)
+            {
+                return DRAW_CIRCLE;
+            } else if(rectangleRadio.Checked)
+            {
+                return DRAW_RECTANGLE;
+            }else if(triangleRadio.Checked)
+            {
+                return DRAW_TRIANGLE;
+            }else if(elipseRadio.Checked)
+            {
+                return DRAW_ELIPSE;
+            }else if(pentagonRadio.Checked)
+            {
+                return DRAW_PENTAGON;
+            }else if(hexagonRadio.Checked)
+            {
+                return DRAW_HEXAGON;
+            }
+            return DRAW_LINE;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pStart.X = 0;
+            pStart.Y = 0;
+            pEnd.X = 0;
+            pEnd.Y = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+        }
+
+        private void drawEqualPolygon(int egdeNum) // ve da giac deu cho so canh
+        {
+            OpenGL gl = openGLControl.OpenGL;
+            gl.Begin(OpenGL.GL_LINE_LOOP);
+            double x1 = pStart.X, x2 = pEnd.X, y1 = gl.RenderContextProvider.Height - pStart.Y, y2 = gl.RenderContextProvider.Height - pEnd.Y,
+              AB = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            double r = Math.Sqrt(Math.Pow(y2 - y1, 2) + Math.Pow(x2 - x1, 2)),
+                alpha = Math.Atan2((x2 - x1), (y2 - y1));
+            for (double ii = 0; ii < egdeNum; ii++)
+            {
+                double theta = (2.0f * pi * ii / egdeNum + pi / 2 - alpha);//get the current angle 
+                double x = r * Math.Cos(theta);//calculate the x component 
+                double y = r * Math.Sin(theta);//calculate the y component 
+                gl.Vertex(x1 + x, y1 + y);//output vertex 
+            }
+            gl.End();
+            gl.Flush();// Thực hiện lệnh vẽ ngay lập tức thay vì đợi sau 1 khoảng thời gian
+
+        }
+
+        private void openGLControl_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Clear the color and depth buffer.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.Color(colorDialog1.Color.R / 255.0, colorDialog1.Color.G / 255.0, colorDialog1.Color.B / 255.0, 0.0f);
+            gl.LineWidth((float)numericUpDown1.Value);
+            
+            int drawOp = getDrawOption();
+            if(drawOp == DRAW_LINE)
+            {
+                gl.Begin(OpenGL.GL_LINES);
+                gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pStart.Y);
+                gl.Vertex(pEnd.X, gl.RenderContextProvider.Height - pEnd.Y);
+                gl.End();
+
+                gl.Flush();
+            }
+            else if(drawOp == DRAW_CIRCLE)
+            {
+                drawEqualPolygon(450);
+            }
+            else if(drawOp == DRAW_RECTANGLE)
+            {
+                gl.Begin(OpenGL.GL_LINE_LOOP);
+                gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pStart.Y);
+                gl.Vertex(pEnd.X, gl.RenderContextProvider.Height - pStart.Y);
+                gl.Vertex(pEnd.X, gl.RenderContextProvider.Height - pEnd.Y);
+                gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pEnd.Y);
+                gl.End();
+            }
+            else if(drawOp == DRAW_TRIANGLE)
+            {
+                drawEqualPolygon(3);
+            }
+            else if(drawOp == DRAW_PENTAGON)
+            {
+                drawEqualPolygon(5);
+            }
+            else if (drawOp == DRAW_HEXAGON)
+            {
+                drawEqualPolygon(6);
+            }
+            else if(drawOp == DRAW_ELIPSE)
+            {
+                double x1 = pStart.X, x2 = pEnd.X, y1 = gl.RenderContextProvider.Height - pStart.Y, y2 = gl.RenderContextProvider.Height - pEnd.Y;
+                // tinh r1, r2
+                double rx = Math.Abs(x1 - x2), ry = Math.Abs(y1 - y2);
+
+                double i, inc;
+                gl.Begin(OpenGL.GL_LINE_LOOP);
+                inc = pi / Math.Max(rx, ry) / 2;
+                i = 0.0;
+                while (i <= 360.0)
+                {
+                    gl.Vertex(((rx * Math.Cos(i) + y1) + .5), ((ry * Math.Sin(i) + x1) + .5));
+                    i = i + inc;
+                }
+                gl.End();
+            }
+
+            if(ended)
+            {
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                textBox1.Text = elapsedMs.ToString() + " ms";
+                ended = false;
+            }
+        }
+
+        private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Set the clear color.
+            gl.ClearColor(255, 255, 255, 255);
+            // Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            // Load the identity.
+            gl.LoadIdentity();
+        }
+
+        private void openGLControl_Resized(object sender, EventArgs e)
+        { 
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            // Load the identity.
+            gl.LoadIdentity();
+            // Create a perspective transformation.
+            gl.Viewport(0, 0, openGLControl.Width, openGLControl.Height);
+            gl.Ortho2D(0, openGLControl.Width, 0, openGLControl.Height);
+        }
+
+        private void openGLControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            pEnd = e.Location;
+            started = false;
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            ended = true;
+        }
+
+        private void openGLControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            pStart = e.Location;
+            pEnd = pStart;
+            started = true;
+        }
+
+        private void openGLControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (started)
+            {
+                pEnd = e.Location;
+            }
+        }
+    }
+}
